1040c399f13f228292b3f9bf4dce21f3cd900792
Vincent LJ <vincent.ljeune@gmail.com>
2023-12-23 15:01:08 +0100

    chore: bump feat version

--- compute/__init__.py
+++ compute/__init__.py
@@ -19,11 +19,10 @@ import pow
 
 # Import all submodules.
 import protocol
-import reward
 import util
 
 # Define the version of the template module.
-__version__ = "1.0.5"
+__version__ = "1.1.0"
 version_split = __version__.split(".")
 __spec_version__ = (1000 * int(version_split[0])) + (10 * int(version_split[1])) + (1 * int(version_split[2]))
 
