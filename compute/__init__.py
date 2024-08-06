ddc390b5198882570f1320b35996d4a03c5c3ef9
nauttiilus <160866334+nauttiilus@users.noreply.github.com>
2024-08-06 16:52:55 +0200

    chore: bump version to 1.4.6

--- compute/__init__.py
+++ compute/__init__.py
@@ -18,7 +18,7 @@
 import string
 
 # Define the version of the template module.
-__version__ = "1.4.5"
+__version__ = "1.4.6"
 __minimal_miner_version__ = "1.4.5"
 __minimal_validator_version__ = "1.4.5"
 
