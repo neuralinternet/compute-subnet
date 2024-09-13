9332412a3675cf239c09b1d9fdc90a8e944737cf
Nam Nguyen <bigsat309@gmail.com>
2024-09-13 14:10:21 +0800

    fix(http_server.py): kill the process before starting server

--- neurons/Miner/http_server.py
+++ neurons/Miner/http_server.py
@@ -1,9 +1,26 @@
 import http.server
 import socketserver
 from socketserver import TCPServer
+import subprocess
 import threading
 
+def kill_process_on_port(port):
+    # Find the process using the port
+    find_process = f"sudo lsof -t -i:{port}"
+    try:
+        process_id = subprocess.check_output(find_process, shell=True).strip()
+        if process_id:
+            # Kill the process using the port
+            subprocess.run(f"sudo kill -9 {process_id.decode()}", shell=True)
+            print(f"Process on port {port} has been killed.")
+        else:
+            print(f"No process found using port {port}.")
+    except subprocess.CalledProcessError:
+        print(f"Port {port} is not in use.")        
+
 def start_server(port) -> TCPServer:
+    kill_process_on_port(port)
+    
     handler = http.server.SimpleHTTPRequestHandler
     httpd: TCPServer = socketserver.TCPServer(("", int(port)), handler)
     
@@ -15,3 +32,5 @@ def start_server(port) -> TCPServer:
 
 def stop_server(httpd: TCPServer) -> None:
     httpd.shutdown()
+    httpd.server_close() 
+    
