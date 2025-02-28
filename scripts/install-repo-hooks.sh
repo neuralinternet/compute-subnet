3848a68907cf9997da95b9a3561acc5ab6527363
David Maldonado <gorossy07@gmail.com>
2025-02-28 13:15:53 -0500

    feat: initial setup pytest and py-coverage

--- scripts/install-repo-hooks.sh
+++ scripts/install-repo-hooks.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python3 -m pip install pre-commit
+python3 -m pip install pytest pytest-cov pre-commit
 pre-commit install --install-hooks
 pre-commit install -t commit-msg
 pre-commit install -t post-checkout
